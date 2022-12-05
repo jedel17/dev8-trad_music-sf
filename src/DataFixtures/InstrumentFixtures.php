80d08c14b70b42af33b6bdc51643b832dfe7b474
jdel17 <jessica.delourme@sfr.fr>
2022-12-05 11:52:28 +0100

    Intallation de webpack

--- src/DataFixtures/InstrumentFixtures.php
+++ src/DataFixtures/InstrumentFixtures.php
@@ -6,23 +6,24 @@ use App\Entity\Instrument;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
-
-// ajout des instruments
 class InstrumentFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-         $flute = new Instrument(); //pour ajouter un instrument
-        $flute->setName('Flute'); //pour envoyer les données
-        $manager->persist($flute);//php
+        $flute = new Instrument();
+        $flute->setName('Flute');
+        $this->addReference('instrument-flute', $flute);
+        $manager->persist($flute);
 
         $guitar = new Instrument();
         $guitar->setName('Guitar');
         $guitar->setIcon('fa-solid fa-guitar');
+        $this->addReference('instrument-guitar', $guitar);
         $manager->persist($guitar);
 
         $fiddle = new Instrument();
         $fiddle->setName('Fiddle');
+        $this->addReference('instrument-fiddle', $fiddle);
         $manager->persist($fiddle);
 
         $manager->flush();
